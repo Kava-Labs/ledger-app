b0c778364202f84a15065282ac1a76a3617f6be9
Federico Kunze <federico.kunze94@gmail.com>
2020-07-22 21:19:42 +0200

    mappings for parser

--- app/src/coin.h
+++ app/src/coin.h
@@ -16,43 +16,58 @@
 #pragma once
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include <stdint.h>
 #include <stddef.h>
 
-#define CLA                  0x55
+#define CLA 0x55
 
-#define HDPATH_LEN_DEFAULT   5
+#define HDPATH_LEN_DEFAULT 5
 
-#define HDPATH_0_DEFAULT     (0x80000000u | 0x2cu)
-#define HDPATH_1_DEFAULT     (0x80000000u | 0x76u)
-#define HDPATH_2_DEFAULT     (0x80000000u | 0u)
-#define HDPATH_3_DEFAULT     (0u)
-#define HDPATH_4_DEFAULT     (0u)
+#define HDPATH_0_DEFAULT (0x80000000u | 0x2cu)
+#define HDPATH_1_DEFAULT (0x80000000u | 0x76u)
+#define HDPATH_2_DEFAULT (0x80000000u | 0u)
+#define HDPATH_3_DEFAULT (0u)
+#define HDPATH_4_DEFAULT (0u)
 
-#define PK_LEN_SECP256K1     33u
+#define PK_LEN_SECP256K1 33u
 
-typedef enum {
-    addr_secp256k1 = 0,
-} address_kind_e;
+    typedef enum
+    {
+        addr_secp256k1 = 0,
+    } address_kind_e;
 
-#define VIEW_ADDRESS_OFFSET_SECP256K1       PK_LEN_SECP256K1
-#define VIEW_ADDRESS_LAST_PAGE_DEFAULT      0
+#define VIEW_ADDRESS_OFFSET_SECP256K1 PK_LEN_SECP256K1
+#define VIEW_ADDRESS_LAST_PAGE_DEFAULT 0
 
-#define MENU_MAIN_APP_LINE1                "Cosmos"
-#define MENU_MAIN_APP_LINE2                "ready"
-#define APPVERSION_LINE1                   "Version:"
-#define APPVERSION_LINE2                   ("v" APPVERSION)
+#define MENU_MAIN_APP_LINE1 "Kava"
+#define MENU_MAIN_APP_LINE2 "ready"
+#define APPVERSION_LINE1 "Version:"
+#define APPVERSION_LINE2 ("v" APPVERSION)
 
-#define CRYPTO_BLOB_SKIP_BYTES              0
-#define COIN_DEFAULT_CHAINID                "cosmoshub-3"
+#define CRYPTO_BLOB_SKIP_BYTES 0
+#define COIN_DEFAULT_CHAINID "kava-3"
 
-// In non-expert mode, the app will convert from uatom to ATOM
-#define COIN_DEFAULT_DENOM_BASE             "uatom"
-#define COIN_DEFAULT_DENOM_REPR             "ATOM"
-#define COIN_DEFAULT_DENOM_FACTOR           6
+    // In non-expert mode, the app will convert from denom base to denom repr
+
+#define KAVA_DENOM_BASE "ukava"
+#define KAVA_DENOM_REPR "KAVA"
+#define KAVA_DENOM_FACTOR 6
+
+#define USDX_DENOM_BASE "usdx"
+#define USDX_DENOM_REPR "USDX"
+#define USDX_DENOM_FACTOR 2
+
+#define ATOM_DENOM_BASE "uatom"
+#define ATOM_DENOM_REPR "ATOM"
+#define ATOM_DENOM_FACTOR 6
+
+#define BNB_DENOM_BASE "bnb"
+#define BNB_DENOM_REPR "BNB"
+#define BNB_DENOM_FACTOR 6
 
 #ifdef __cplusplus
 }
--- app/src/parser.c
+++ app/src/parser.c
@@ -25,12 +25,14 @@
 
 parser_error_t parser_parse(parser_context_t *ctx,
                             const uint8_t *data,
-                            size_t dataLen) {
+                            size_t dataLen)
+{
     CHECK_PARSER_ERR(tx_display_readTx(ctx, data, dataLen))
     return parser_ok;
 }
 
-parser_error_t parser_validate(const parser_context_t *ctx) {
+parser_error_t parser_validate(const parser_context_t *ctx)
+{
     CHECK_PARSER_ERR(tx_validate(&parser_tx_obj.json))
 
     // Iterate through all items to check that all can be shown and are valid
@@ -40,7 +42,8 @@ parser_error_t parser_validate(const parser_context_t *ctx) {
     char tmpKey[40];
     char tmpVal[40];
 
-    for (uint8_t idx = 0; idx < numItems; idx++) {
+    for (uint8_t idx = 0; idx < numItems; idx++)
+    {
         uint8_t pageCount = 0;
         CHECK_PARSER_ERR(parser_getItem(ctx, idx, tmpKey, sizeof(tmpKey), tmpVal, sizeof(tmpVal), 0, &pageCount))
     }
@@ -48,28 +51,35 @@ parser_error_t parser_validate(const parser_context_t *ctx) {
     return parser_ok;
 }
 
-parser_error_t parser_getNumItems(const parser_context_t *ctx, uint8_t *num_items) {
+parser_error_t parser_getNumItems(const parser_context_t *ctx, uint8_t *num_items)
+{
     *num_items = 0;
     return tx_display_numItems(num_items);
 }
 
-__Z_INLINE bool_t parser_areEqual(uint16_t tokenidx, char *expected) {
-    if (parser_tx_obj.json.tokens[tokenidx].type != JSMN_STRING) {
+__Z_INLINE bool_t parser_areEqual(uint16_t tokenidx, char *expected)
+{
+    if (parser_tx_obj.json.tokens[tokenidx].type != JSMN_STRING)
+    {
         return bool_false;
     }
 
     int16_t len = parser_tx_obj.json.tokens[tokenidx].end - parser_tx_obj.json.tokens[tokenidx].start;
-    if (len < 0) {
+    if (len < 0)
+    {
         return bool_false;
     }
 
-    if (strlen(expected) != (size_t) len) {
+    if (strlen(expected) != (size_t)len)
+    {
         return bool_false;
     }
 
     const char *p = parser_tx_obj.tx + parser_tx_obj.json.tokens[tokenidx].start;
-    for (uint16_t i = 0; i < len; i++) {
-        if (expected[i] != *(p + i)) {
+    for (uint16_t i = 0; i < len; i++)
+    {
+        if (expected[i] != *(p + i))
+        {
             return bool_false;
         }
     }
@@ -77,7 +87,8 @@ __Z_INLINE bool_t parser_areEqual(uint16_t tokenidx, char *expected) {
     return bool_true;
 }
 
-__Z_INLINE bool_t parser_isAmount(char *key) {
+__Z_INLINE bool_t parser_isAmount(char *key)
+{
     if (strcmp(key, "fee/amount") == 0)
         return bool_true;
 
@@ -90,15 +101,27 @@ __Z_INLINE bool_t parser_isAmount(char *key) {
     if (strcmp(key, "msgs/value/amount") == 0)
         return bool_true;
 
+    if (strcmp(key, "msgs/value/collateral") == 0)
+        return bool_true;
+
+    if (strcmp(key, "msgs/value/principal") == 0)
+        return bool_true;
+
+    if (strcmp(key, "msgs/value/payment") == 0)
+        return bool_true;
+
     return bool_false;
 }
 
-__Z_INLINE bool_t is_default_denom_base(const char *denom, uint8_t denom_len) {
-    if (tx_is_expert_mode()){
+__Z_INLINE bool_t is_default_denom_base(const char *denom, uint8_t denom_len)
+{
+    if (tx_is_expert_mode())
+    {
         return false;
     }
 
-    if (strlen(COIN_DEFAULT_DENOM_BASE) != denom_len) {
+    if (strlen(COIN_DEFAULT_DENOM_BASE) != denom_len)
+    {
         return bool_false;
     }
 
@@ -110,9 +133,11 @@ __Z_INLINE bool_t is_default_denom_base(const char *denom, uint8_t denom_len) {
 
 __Z_INLINE parser_error_t parser_formatAmount(uint16_t amountToken,
                                               char *outVal, uint16_t outValLen,
-                                              uint8_t pageIdx, uint8_t *pageCount) {
+                                              uint8_t pageIdx, uint8_t *pageCount)
+{
     *pageCount = 0;
-    if (parser_tx_obj.json.tokens[amountToken].type == JSMN_ARRAY) {
+    if (parser_tx_obj.json.tokens[amountToken].type == JSMN_ARRAY)
+    {
         amountToken++;
     }
 
@@ -120,7 +145,8 @@ __Z_INLINE parser_error_t parser_formatAmount(uint16_t amountToken,
 
     CHECK_PARSER_ERR(array_get_element_count(&parser_tx_obj.json, amountToken, &numElements));
 
-    if (numElements == 0) {
+    if (numElements == 0)
+    {
         *pageCount = 1;
         snprintf(outVal, outValLen, "Empty");
         return parser_ok;
@@ -143,7 +169,8 @@ __Z_INLINE parser_error_t parser_formatAmount(uint16_t amountToken,
     MEMZERO(bufferUI, sizeof(bufferUI));
 
     const char *amountPtr = parser_tx_obj.tx + parser_tx_obj.json.tokens[amountToken + 2].start;
-    if (parser_tx_obj.json.tokens[amountToken + 2].start < 0) {
+    if (parser_tx_obj.json.tokens[amountToken + 2].start < 0)
+    {
         return parser_unexpected_buffer_end;
     }
 
@@ -153,35 +180,43 @@ __Z_INLINE parser_error_t parser_formatAmount(uint16_t amountToken,
     const int16_t denomLen = parser_tx_obj.json.tokens[amountToken + 4].end -
                              parser_tx_obj.json.tokens[amountToken + 4].start;
 
-    if (amountLen <= 0) {
+    if (amountLen <= 0)
+    {
         return parser_unexpected_buffer_end;
     }
 
-    if (denomLen <= 0) {
+    if (denomLen <= 0)
+    {
         return parser_unexpected_buffer_end;
     }
 
-    if (sizeof(bufferUI) < (size_t) (amountLen + denomLen + 2) ) {
+    if (sizeof(bufferUI) < (size_t)(amountLen + denomLen + 2))
+    {
         return parser_unexpected_buffer_end;
     }
 
-    if (is_default_denom_base(denomPtr, denomLen)) {
+    if (is_default_denom_base(denomPtr, denomLen))
+    {
         // Then we convert denomination
         char tmp[50];
-        if (amountLen < 0 || ((uint16_t) amountLen) >= sizeof(tmp)) {
+        if (amountLen < 0 || ((uint16_t)amountLen) >= sizeof(tmp))
+        {
             return parser_unexpected_error;
         }
         MEMZERO(tmp, sizeof(tmp));
         MEMCPY(tmp, amountPtr, amountLen);
 
-        if (fpstr_to_str(bufferUI, sizeof(tmp), tmp, COIN_DEFAULT_DENOM_FACTOR)!=0) {
+        if (fpstr_to_str(bufferUI, sizeof(tmp), tmp, COIN_DEFAULT_DENOM_FACTOR) != 0)
+        {
             return parser_unexpected_error;
         }
 
-        const uint16_t formatted_len =strlen(bufferUI);
+        const uint16_t formatted_len = strlen(bufferUI);
         bufferUI[formatted_len] = ' ';
         MEMCPY(bufferUI + 1 + formatted_len, COIN_DEFAULT_DENOM_REPR, strlen(COIN_DEFAULT_DENOM_REPR));
-    } else {
+    }
+    else
+    {
         MEMCPY(bufferUI, amountPtr, amountLen);
         bufferUI[amountLen] = ' ';
         MEMCPY(bufferUI + 1 + amountLen, denomPtr, denomLen);
@@ -196,7 +231,8 @@ parser_error_t parser_getItem(const parser_context_t *ctx,
                               uint16_t displayIdx,
                               char *outKey, uint16_t outKeyLen,
                               char *outVal, uint16_t outValLen,
-                              uint8_t pageIdx, uint8_t *pageCount) {
+                              uint8_t pageIdx, uint8_t *pageCount)
+{
     *pageCount = 0;
 
     MEMZERO(outKey, outKeyLen);
@@ -206,11 +242,13 @@ parser_error_t parser_getItem(const parser_context_t *ctx,
     CHECK_PARSER_ERR(parser_getNumItems(ctx, &numItems))
     CHECK_APP_CANARY()
 
-    if (numItems == 0) {
+    if (numItems == 0)
+    {
         return parser_unexpected_number_items;
     }
 
-    if (displayIdx < 0 || displayIdx >= numItems) {
+    if (displayIdx < 0 || displayIdx >= numItems)
+    {
         return parser_display_idx_out_of_range;
     }
 
@@ -218,25 +256,30 @@ parser_error_t parser_getItem(const parser_context_t *ctx,
     CHECK_PARSER_ERR(tx_display_query(displayIdx, outKey, outKeyLen, &ret_value_token_index));
     CHECK_APP_CANARY()
 
-    if (parser_isAmount(outKey)) {
+    if (parser_isAmount(outKey))
+    {
         CHECK_PARSER_ERR(parser_formatAmount(
-                ret_value_token_index,
-                outVal, outValLen,
-                pageIdx, pageCount))
-    } else {
+            ret_value_token_index,
+            outVal, outValLen,
+            pageIdx, pageCount))
+    }
+    else
+    {
         CHECK_PARSER_ERR(tx_getToken(
-                ret_value_token_index,
-                outVal, outValLen,
-                pageIdx, pageCount))
+            ret_value_token_index,
+            outVal, outValLen,
+            pageIdx, pageCount))
     }
     CHECK_APP_CANARY()
 
     CHECK_PARSER_ERR(tx_display_make_friendly())
     CHECK_APP_CANARY()
 
-    if (*pageCount > 1) {
+    if (*pageCount > 1)
+    {
         size_t keyLen = strlen(outKey);
-        if (keyLen < outKeyLen) {
+        if (keyLen < outKeyLen)
+        {
             snprintf(outKey + keyLen, outKeyLen - keyLen, " [%d/%d]", pageIdx + 1, *pageCount);
         }
     }
